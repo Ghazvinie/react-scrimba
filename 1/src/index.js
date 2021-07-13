01aed7a8c581c3d8be319a46b73e4d09c874f565
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-13 21:14:08 +0100

    list

--- 1/src/index.js
+++ 1/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
+const MyList = () => (
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+)
+
 ReactDOM.render(
-  <App title="The GitHub Cards App" />,
+  <MyList />,
   document.getElementById('root'),
 );
 
