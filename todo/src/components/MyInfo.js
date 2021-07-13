b701cdb34e9809fb6a8d7648ea65b54059a66b04
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-13 22:18:07 +0100

    to do setup

---
+++ todo/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const MyInfo = () => (
+    <React.Fragment>
+      <h1>Daniel</h1>
+      <p>Hello, I am a being. I live on a planet in the universe. I like to move my arms.</p>
+      <ol>I'd love to travel to:
+        <li>Bizzo (()3</li>
+        <li>St. Hurg 9Teene</li>
+        <li>The Death Pits of Zultron</li>
+        <li>FOUR hUNDREAD SEASONS</li>
+      </ol>
+    </React.Fragment>
+  )
+
+export default MyInfo
