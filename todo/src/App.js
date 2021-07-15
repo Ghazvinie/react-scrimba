6a16f8c977c6e0278a235c619a983d2f9905259f
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-15 16:17:56 +0100

    added checkboxes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Checkbox from './components/Checkbox';
 
 const App = () => (
     <div>
-
+        <Checkbox text={'Some Text'}/>
+        <Checkbox text={'Some Text'}/>
+        <Checkbox text={'Some Text'}/>
+        <Checkbox text={'Some Text'}/>
     </div>
 )
 
--- todo/src/components/Checkbox.js
+++ todo/src/components/Checkbox.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Checkbox = () => (
- 
+const Checkbox = (props) => (
+    <div class="checkboxes-containter">
+        <div class="checkbox-item">
+            <p>{props.text}</p>
+            <div class="checkbox-single-container">
+                <input type="checkbox"/>
+            </div>
+        </div>
+    </div>
+
 );
 
 export default Checkbox;
