38b9508839f7a6f1b668dc2601bb35ea010d6512
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-15 21:21:33 +0100

    added random jokes generation

--- joke/src/components/Joke.js
+++ joke/src/components/Joke.js
@@ -5,6 +5,7 @@ const Joke = (props) => {
     while (randomJokes.length < 5) {
         const rdmIdx = Math.floor(Math.random() * props.jokes.length);
         randomJokes.push(props.jokes[rdmIdx]);
+        randomJokes = randomJokes.filter((joke, index) => randomJokes.indexOf(joke) === index);
     }
     return randomJokes.map(joke => {
         return (
