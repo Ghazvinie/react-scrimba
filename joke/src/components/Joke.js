eb4dcc88201cb1075fdc6a298f06c3a7d7b87829
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-15 19:48:20 +0100

    adding joke component

--- joke/src/components/Joke.js
+++ joke/src/components/Joke.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Joke = (props) => {
+    return (
+        <div className='joke-container'>
+            <div className='joke-single'>
+                <div className="joke-setup">
+                    <p >Why Did the?</p>
+                </div>
+                <div className="punchline">
+                    <p >The Punchline</p>
+                </div>
+            </div>
 
+        </div>
+    )
 }
 
 
