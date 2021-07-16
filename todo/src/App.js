a4b283baec25908becd4505b91751d4b167fafa4
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-16 20:10:31 +0100

    changed app to class component

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import ToDoItem from './components/ToDoItem';
 import toDoData from './todosData';
 
-const App = () => {
-    return (
-        <div className="todo-items-container">
-            <ToDoItem text={toDoData} />
-        </div>
-    )
-};
+class App  extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            toDoData: toDoData
+        };
+    };
+    render () {
+        return (
+            <div className="todo-items-container">
+                <ToDoItem text={this.state.toDoData} />
+            </div>
+        )
+    }
+}
 
 export default App;
