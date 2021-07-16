59052811d87c7faa840c19e2e84b02c4b1aa9e45
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-16 20:44:23 +0100

    adding event handlers

--- todo/src/components/ToDoItem.js
+++ todo/src/components/ToDoItem.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-const ToDoItem = (props) => {
-    return props.text.map(item => (
-        <div className="todo-item" key={item.id}>
-            <p>{item.text}</p>
-            <div className="checkbox-single-container">
-                <input type="checkbox" checked={item.completed} />
-            </div>
-        </div>
-    ));
+class ToDoItem extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isChecked: false
+        };
+    };
+
+    handleClick(e) {
+        this.state.isChecked = false;
+    }
+
+    render() {
+        return this.props.text.map(item => {
+            this.state.isChecked = item.completed;
+            return (
+                <div className="todo-item" key={item.id} >
+                    <p>{item.text}</p>
+                    <div className="checkbox-single-container" >
+                        <input type="checkbox" checked={this.state.isChecked} onChange={this.handleClick} />
+                    </div>
+                </div>
+            );
+        });
+    }
 }
+
 
 
 export default ToDoItem;
