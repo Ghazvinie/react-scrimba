6bf034b7f8270555e33ae2c4df84cd5b630fcde2
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-16 17:28:37 +0100

    adding product list

---
+++ product-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import productsData from "./vschoolProducts"
+import Product from './Product.js'
+
+function App() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default App
---
+++ product-list/src/components/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Product = (props) => {
+    return props.products.map(product => (
+        <div>
+        </div>
+    ))
+}
