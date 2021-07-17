a14b93ef9c228a23bf46cddabce5475eda6e0cf5
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-17 15:38:26 +0100

    added checkbox state handler

--- todo/src/components/ToDoItem.js
+++ todo/src/components/ToDoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class ToDoItem extends React.Component {
     constructor(props) {
-        super()
+        super(props)
         this.state = {
             toDos: props.text
         };
