686af887f564525cefcef6240d225498cb84574a
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-19 18:54:17 +0100

    forms practice

--- swapi/src/components/Character.js
+++ swapi/src/components/Character.js
@@ -5,8 +5,13 @@ class Character extends React.Component {
         super()
         this.state = {
             character: {},
-            isLoading: false
+            isLoading: false,
+            firstName: '',
+            lastName: ''
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit(this);
     };
 
     async componentDidMount() {
@@ -15,17 +20,38 @@ class Character extends React.Component {
         const response = await fetch('https://swapi.dev/api/people/1/');
         const data = await response.json();
 
-
         this.setState({
             character: data,
             isLoading: false
         });
     };
 
+    handleChange(e) {
+        this.setState({ formValue: e.target.value })
+    }
+
+    handleSubmit(e){
+
+        console.log('Form was submitted' + this.state.value);
+
+    }
+
     render() {
         return (
             <div>
-                <h1>{this.state.isLoading ? 'Page Loading...' : this.state.character.name}</h1>
+
+                {/* <h1>{this.state.isLoading ? 'Page Loading...' : this.state.character.name}</h1> */}
+                <form onSubmit={(e)=> this.handleSubmit(e)}>
+                    <label>
+                        Name:
+                        <input type="text" name='firstName' onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input type="text" name='lastName' onChange={this.handleChange} />
+                    </label>
+                    <input type='submit' value='submit' />
+                </form>
             </div>
         );
     };
