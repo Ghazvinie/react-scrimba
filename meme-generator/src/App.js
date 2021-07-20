9babde37942134e758a39ac70185a271dc3ac957
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-20 20:48:36 +0100

    setting up meme generator

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Head from './components/Header';
+import MemeGenerator from "./components/MemeGenerator";
 
 const App = () => (
     <div>
---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+
+        };
+    };
+};
+
+export default Header;
