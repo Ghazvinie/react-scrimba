506994ff6b79dcb9c44a335e616c12468ec37cd5
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-20 16:38:22 +0100

    added radio gender

--- forms/src/components/Form.js
+++ forms/src/components/Form.js
@@ -86,12 +86,40 @@ import React from 'react';
 //     };
 // };
 
+
+
+/**
+ * Challenge: Wire up the partially-finished travel form so that it works!
+ * Remember to use the concept of controlled forms
+ * https://reactjs.org/docs/forms.html
+ * 
+ * All information should be populating the text below the form in real-time
+ * as you're filling it out
+ * 
+ * This exercise is adapted from the V School curriculum on vanilla JS forms:
+ * https://coursework.vschool.io/travel-form/
+ * 
+ * All of our challenges and learning resources are open for the public
+ * to play around with and learn from at https://coursework.vschool.io
+ */
+
 class Form extends React.Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            gender: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
     }
-    
+
+    handleChange(e) {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name] : value
+        });
+    }
+
     render() {
         return (
             <main>
@@ -99,26 +127,37 @@ class Form extends React.Component {
                     <input placeholder="First Name" /><br />
                     <input placeholder="Last Name" /><br />
                     <input placeholder="Age" /><br />
-                    
+
                     {/* Create radio buttons for gender here */}
+                    <input type='radio'
+                        name='gender'
+                        value='Male'
+                        checked={this.state.gender === 'Male'}
+                        onChange={this.handleChange} /> Male
+                    <input type='radio'
+                        name='gender'
+                        value='Female'
+                        checked={this.state.gender === 'Female'}
+                        onChange={this.handleChange} /> Female
+
                     <br />
-                    
+
                     {/* Create select box for location here */}
                     <br />
-                    
+
                     {/* Create check boxes for dietary restrictions here */}
                     <br />
-                    
+
                     <button>Submit</button>
                 </form>
                 <hr />
                 <h2>Entered information:</h2>
                 <p>Your name: {/* First and last name here */}</p>
                 <p>Your age: {/* Age here */}</p>
-                <p>Your gender: {/* Gender here */}</p>
+                <p>Your gender: {this.state.gender}</p>
                 <p>Your destination: {/* Destination here */}</p>
                 <p>
-                    Your dietary restrictions: 
+                    Your dietary restrictions:
                     {/* Dietary restrictions here, comma separated */}
                 </p>
             </main>
