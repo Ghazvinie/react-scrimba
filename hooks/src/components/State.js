d9a5c24030369767e7ec46ba0216b29cfa36859c
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-07-23 17:21:58 +0100

    useState()  practice more state changes

--- hooks/src/components/State.js
+++ hooks/src/components/State.js
@@ -7,6 +7,8 @@ function State() {
         <div>
             <h1>{count}</h1>
             <button onClick={()=> setCount(count+1)}>Increment</button>
+            <button onClick={() => setCount(count*2)}>Double</button>
+            <button onClick={() => setCount(Math.round(count/2))}>Half</button>
             <button onClick={() => setCount(count-1)}>Decrement</button>
         </div>
     );
