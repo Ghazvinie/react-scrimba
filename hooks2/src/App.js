3c3ad133b6688a901f20d0c69698e200ab97c260
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-02 20:11:04 +0100

    hooks practice

--- hooks2/src/App.js
+++ hooks2/src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
-const App = () => (
-    <div>
-        Hello
-    </div>
-)
+
+function App() {
+    const [count, setCount] = useState(0)
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Change!</button>
+        </div>
+    )
+}
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             answer: "Yes"
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Is state important to know? {this.state.answer}</h1>
+//             </div>
+//         )
+//     }
+// }
 
 export default App;
