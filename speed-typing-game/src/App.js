4cadeaab3ae466518749aef1d8882d27ca286cc4
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-03 18:11:45 +0100

    speed typing game timer

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
     const [text, setText] = useState('');
+    const [time, setTime] = useState(null);
+    const [timeSet, setTimeSet] = useState('');
 
     function handleTextChange(e) {
         const { value } = e.target;
         setText(value);
     }
 
-    function countWords() {
+    function countWords(text) {
         const numWords = text
             .trim()
             .split(' ')
@@ -18,13 +20,33 @@ const App = () => {
         return numWords;
     }
 
+    useEffect(() => {
+        if (time <= 0 || !time) return;
+        setTimeout(() => {
+            setTime((prevTime) => prevTime - 1);
+        }, 1000);
+
+    }, [time]);
+
+    function setTimer(e) {
+        if (time > 0 && time != null) return;
+        const { value } = e.target;
+        setTimeSet(value);
+    }
+
+    function handleSetTime(){
+        if (time > 0) return;
+        setTime(timeSet);
+    }
+
     return (
         <div>
             <h1>Crazy Game!</h1>
             <textarea value={text} name='textArea' onChange={(e) => handleTextChange(e)} />
-            <h4>Time Remaining</h4>
-            <button>Start</button>
-            <h1>Word Count:{countWords()}</h1>
+            <input type='text' value={timeSet} onChange={(e) => setTimer(e)} /> Set Timer
+            <h4>Time Remaining: {time}</h4>
+            <button onClick={() => handleSetTime()}>Start</button>
+            <h1>Word Count: {countWords(text)}</h1>
         </div>
     );
 }
