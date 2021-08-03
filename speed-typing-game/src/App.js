728bcf37f993fe24d36d393b6bb03ad053655bae
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-03 17:33:56 +0100

    speed typing game word count

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -4,18 +4,27 @@ const App = () => {
     const [text, setText] = useState('');
 
     function handleTextChange(e) {
-        const {value} = e.target;
+        const { value } = e.target;
         setText(value);
-        console.log(value)
+    }
+
+    function countWords() {
+        const numWords = text
+            .trim()
+            .split(' ')
+            .filter(element => element !== '')
+            .length;
+
+        return numWords;
     }
 
     return (
         <div>
             <h1>Crazy Game!</h1>
-            <textarea value={text} name='textArea' onChange={(e)=>handleTextChange(e)}/>
+            <textarea value={text} name='textArea' onChange={(e) => handleTextChange(e)} />
             <h4>Time Remaining</h4>
             <button>Start</button>
-            <h1>Word Count</h1>
+            <h1>Word Count:{countWords()}</h1>
         </div>
     );
 }
