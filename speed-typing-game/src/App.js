876ea8745a3875e08f22bd3d4c2c109afa911b1c
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-03 17:25:30 +0100

    speed typing game textarea

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = () => (
-    <div>
-        <h1>Crazy Game!</h1>
-        <textarea />
-        <h4>Time Remaining</h4>
-        <button>Start</button>
-        <h1>Word Count</h1>
-    </div>
-)
+const App = () => {
+    const [text, setText] = useState('');
+
+    function handleTextChange(e) {
+        const {value} = e.target;
+        setText(value);
+        console.log(value)
+    }
+
+    return (
+        <div>
+            <h1>Crazy Game!</h1>
+            <textarea value={text} name='textArea' onChange={(e)=>handleTextChange(e)}/>
+            <h4>Time Remaining</h4>
+            <button>Start</button>
+            <h1>Word Count</h1>
+        </div>
+    );
+}
 
 export default App;
