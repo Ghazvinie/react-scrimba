66082779cb8a43712749bd7601159d4a02709255
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-16 19:28:21 +0100

    setting up routes

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 const App = () => (
     <div>
-hello
+        <Link to='/' >Home</Link>
+        <Link to='/about'>About</Link>
+
+        <Switch>
+             <Route exact path='/'><Home/></Route>
+             <Route exact path='/about'><About/></Route>
+        </Switch>
     </div>
 )
 
--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App'
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root'),
 );
 
