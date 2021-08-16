808e43ae1ae852160053ccc846018175984f4938
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-08-16 20:00:11 +0100

    basic nav bar

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Link, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contacts from "./components/Contacts";
 
 const App = () => (
     <div>
-        <Link to='/' >Home</Link>
-        <Link to='/about'>About</Link>
+        <nav>
+            <ul><Link to='/'>Home</Link></ul>
+            <ul><Link to='/about'>About</Link></ul>
+            <ul><Link to='/contacts'>Contacts</Link></ul>
+        </nav>
 
         <Switch>
-             <Route exact path='/'><Home/></Route>
-             <Route exact path='/about'><About/></Route>
+            <Route exact path='/'>
+                <Home/>
+            </Route>
+            <Route exact path='/about'>
+                <About/>
+            </Route>
+            <Route exact path='/contacts'>
+                <Contacts/>
+            </Route>
         </Switch>
+
+
     </div>
 )
 
