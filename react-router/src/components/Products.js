592e511539957f3ef650b96cfb787da747444254
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-16 08:09:25 +0100

    react router challenge

--- react-router/src/components/Products.js
+++ react-router/src/components/Products.js
@@ -2,24 +2,22 @@ import React from "react";
 import productsData from '../pages/products/productsData';
 
 function Products() {
-return (
-    <div>
-    <h1>Products Page</h1>
+    return (
+        <div>
+            <h1>Products Page</h1>
 
-    <ul>
-        {productsData.map(product => {
-            return (
-                <li key={product.id}>
-                <h3>£{product.price} {product.name}</h3>
-                <p>{product.description}</p>
-                </li>
-            )
-        })}
-    </ul>
-
-
-    </div>
-)
+            <ul>
+                {productsData.map(product => {
+                    return (
+                        <li key={product.id}>
+                            <h3>£{product.price} {product.name}</h3>
+                            <p>{product.description}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Products;
