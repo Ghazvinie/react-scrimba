51dd3c669c39df6a18eff05c97a26d6c2de52434
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-17 18:28:43 +0100

    cart and home routes

--- pic-some/src/App.js
+++ pic-some/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Header from "./components/Header";
+import { Switch, Link, Route } from 'react-router-dom';
+import Photos from "./pages/Photos";
+import Cart from './pages/Cart';
 
 const App = () => (
     <div>
-        <h1>hello</h1>
+        <Header />
+        
+        <Switch>
+            <Route exact path='/'>
+                <Photos />
+            </Route>
+            <Route path='/cart'>
+                <Cart />
+            </Route>
+        </Switch>
     </div>
 )
 
--- pic-some/src/index.js
+++ pic-some/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root'),
 );
 
