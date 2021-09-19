2f88e758e04c58ac7feaf1a6adf3dbe6b2616d44
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-19 18:08:05 +0100

    added favourite method

--- pic-some/src/components/Image.js
+++ pic-some/src/components/Image.js
@@ -8,16 +8,18 @@ function Image({ className, img }) {
         <div className={`${className} image-container`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <img src={img.url} className='image-grid' />
             {
-                hover && !img.isFavorite ?
-                <>
-                    <i className="ri-heart-line favorite" onClick={() => toggleFave(img.id)}></i>
-                    <i className="ri-add-circle-line cart"></i>
-                </>
-                :
-                <i className="ri-heart-fill favorite" onClick={() => toggleFave(img.id)}></i>
-
+                img.isFavorite ?
+                    <>
+                        <i className="ri-heart-fill favorite" onClick={() => toggleFave(img.id)}></i>
+                    </>
+                    : hover && !img.isFavorite ?
+                        <>
+                            <i className="ri-heart-line favorite" onClick={() => toggleFave(img.id)}></i>
+                            <i className="ri-add-circle-line cart"></i>
+                        </>
+                        : <></>
             }
-        </div>
+        </div >
     );
 }
 
