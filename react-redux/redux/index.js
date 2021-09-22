bd63cc39206f58d8c5754c67df1fe7c2e8869e72
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-09-22 17:19:19 +0100

    split into separate files

--- react-redux/redux/index.js
+++ react-redux/redux/index.js
@@ -1,7 +1,19 @@
 const redux = require('redux');
-const { combineReducers, store } = redux;
+const { combineReducers, createStore } = redux;
 
 import { youtubeReducer } from './youtubeVideo';
 import { countReducer } from './count';
 import { favieThingsReducer } from './favoriteThings';
 
+const rootReducer = combineReducers({
+    count : countReducer,
+    favieThings: favieThingsReducer,
+    youtubeVideo: youtubeReducer
+});
+
+const store = createStore(rootReducer);
+store.subscribe(() => {
+    console.log(store.sate());
+});
+
+export default store;
